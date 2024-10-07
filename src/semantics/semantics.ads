2b6e744a279c1ad4a4ce505a626864f8f46e4930
Jay Patel <jaypatelani@gmail.com>
2024-10-07 12:56:13 +0530

    Update semantics.ads

--- src/semantics/semantics.ads
+++ src/semantics/semantics.ads
@@ -1,1 +1,18 @@
+with Parser;
 
+package Semantics is
+
+  type AST_Node is aliased Parser.AST_Node;
+
+  procedure Analyze (Tree : in AST_Node;
+                     Errors : out String)
+  pre  Tree /= null;
+  post Errors.Length = 0 or Errors /= "";  -- Empty string for no errors, non-empty for errors
+
+private
+  -- Function prototypes for semantic checks on different AST node types
+  procedure Check_Procedure (Node : in AST_Node; Errors : out String);
+  procedure Check_Statement (Node : in AST_Node; Errors : out String);
+  -- ... (similar functions for other node types)
+
+end Semantics;
