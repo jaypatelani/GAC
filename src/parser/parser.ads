9c63194bbb77e24b4e9a0c98b74d4e8096509646
Jay Patel <jaypatelani@gmail.com>
2024-10-07 12:54:12 +0530

    Update parser.ads

--- src/parser/parser.ads
+++ src/parser/parser.ads
@@ -1,1 +1,26 @@
+with Ada.Text_IO; use Ada.Text_IO;
 
+package Parser is
+
+  type Token_Type is (Keyword, Identifier, Literal, Operator, Other);
+
+  type Token is record
+    Kind  : Token_Type;
+    Image : Ada.Characters.Latin_1.String;
+  end record;
+
+  procedure Parse (Stream : in Ada.Text_IO.Access;
+                   Tree   : out AST_Node)
+  pre  not Stream.Is_Closed;
+  post Tree /= null;  -- Ensure a parse tree is returned
+
+private
+  type AST_Node (abstract);  -- Abstract type for the Abstract Syntax Tree nodes
+
+  -- Function prototypes for parsing different Ada language constructs (procedures, statements, etc.)
+  function Parse_Procedure (Stream : in Ada.Text_IO.Access) return AST_Node;
+  function Parse_Statement (Stream : in Ada.Text_IO.Access) return AST_Node;
+  -- ... (similar functions for other language constructs)
+
+end 
+Parser;
